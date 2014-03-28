ce43d0ed48cf677133e08a668a588ec2a4fc2ec9
Meet Shah <meet1995@gmail.com>
2014-03-28 20:04:43 +0000

    caching setup

--- flak.py
+++ flak.py
@@ -4,19 +4,21 @@ from flask import Flask,jsonify,render_template,request
 import urllib2
 import xmltodict,json
 from cricbuzz import CricbuzzParser
+from werkzeug.contrib.cache import SimpleCache
 
 app=Flask(__name__)
-
+cache=SimpleCache()
 
 
 @app.route('/_get_scores')
 def get_scores():
-    #a = request.args.get('a', 0, type=int)
-    #b = request.args.get('b', 0, type=int)
-    cric = CricbuzzParser()
-    match = cric.getXml()
-    details = cric.handleMatches(match) #Returns Match details as a Dictionary. Parse it according to requirements.
-    o={'details':details}
+    o=cache.get('scores')
+    if o is None:
+      cric = CricbuzzParser()
+      match = cric.getXml()
+      details = cric.handleMatches(match) #Returns Match details as a Dictionary. Parse it according to requirements.
+      o={'details':details}
+      cache.set('scores',o,timeout=15)
     return app.response_class(json.dumps(o,
         indent=None if request.is_xhr else 2), mimetype='application/json')
 
@@ -26,4 +28,4 @@ def index():
     return render_template('index.html')
   
 if __name__=="__main__":
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=8080,debug=False)
